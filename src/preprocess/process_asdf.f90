c57a93f05b90c9160338a339de3a09e37e8bb6f1
James Smith <jas11@princeton.edu>
2014-09-02 11:50:34 -0400

    Initial Commit for preprocessing WORKFLOW

---
+++ src/preprocess/process_asdf.f90
@@ -0,0 +1,283 @@
+module process_asdf_mod
+
+contains
+!> \Time-series Analysis for global adjoint tomography
+!! Developer: Princeton Global Tomography Group(PGTG)
+!! Group Member: James Smith jas11(@princeton.edu), Ebru Bozdag(bozdag@princeton.edu),
+!! Wenjie Lei (lei@princeton.edu)
+!! Bug Report: jas11@princeton.edu
+
+subroutine process_asdf(observed_raw, synthetic_raw, observed_rotate, synthetic_rotate, rank, comm, nproc, adios_group, ierr)
+
+  use mpi
+  use asdf_data
+  use asdf_read_subs
+  use asdf_write_subs
+  use process_var
+  use process_subs
+  use process_subs2
+  use var_main
+
+  implicit none
+
+  integer                   :: comm, rank, ierr, nproc, adios_err
+  integer(kind=8)           :: adios_handle, adios_group
+  integer(kind=8)           :: adios_groupsize, adios_totalsize
+
+  type (asdf_event),intent(inout) :: observed_raw, synthetic_raw
+  type (asdf_event),intent(inout) :: observed_rotate, synthetic_rotate
+  type (asdf_event)               :: observed_proc, synthetic_proc
+
+  !local trace variables(In accordance with SAC lib, the data type is real)
+  real(kind=4) :: b_obsd, b_synt, dt_obsd, dt_synt
+  integer :: npts_obsd, npts_synt
+  real(kind=4) :: obsd(MAX_TRACE_LENGTH), synt(MAX_TRACE_LENGTH)
+
+  !after cut
+  real(kind=4) :: b_obsd_cut, b_synt_cut, dt_obsd_cut, dt_synt_cut
+  integer :: npts_obsd_cut, npts_synt_cut
+  real(kind=4) :: obsd_cut(MAX_TRACE_LENGTH), synt_cut(MAX_TRACE_LENGTH)
+
+  !remove trend and mean
+  integer :: ipts
+  real :: mean, yint, slope, siga, sigb, sig, cc, minnm, maxnm, increment, num
+
+  !instrument response
+  character(len=20) :: sta, net, locid, cha
+
+  !after interpolation
+  real(kind=4) :: b_obsd_interp, b_synt_interp, dt_obsd_interp, dt_synt_interp
+  integer :: npts_obsd_interp, npts_synt_interp
+  real(kind=4) :: obsd_interp(MAX_TRACE_LENGTH), synt_interp(MAX_TRACE_LENGTH)
+  real :: end_time_syn, end_time_obs
+
+  real :: epsi = 1e-8
+
+  !after final cut
+  real(kind=4) :: b_obsd_final, b_synt_final, dt_obsd_final, dt_synt_final
+  integer :: npts_obsd_final, npts_synt_final
+  real(kind=4) :: obsd_final(MAX_TRACE_LENGTH), synt_final(MAX_TRACE_LENGTH)
+
+  integer :: nerr
+
+!-----------------------------------------------------------------------------------
+! READ CMT FILE AND COMPUTE JULIAN DAY AND ORIGIN OF EVENT
+!-----------------------------------------------------------------------------------
+  call read_CMT(observed_raw%event, rank, comm, ierr)
+  call event_origin(rank, comm, ierr)
+
+!-----------------------------------------------------------------------------------
+! ADJUST TIME OF OBSERVED DATA BASED ON CMT EVENT ORIGIN
+!-----------------------------------------------------------------------------------
+  print *, "observed origin time adjusted based on cmt event, rank:", rank
+  call adjust_event_time(observed_raw, ierr)
+
+  call init_asdf_data(observed_proc, observed_raw%nrecords, .false.)
+  call init_asdf_data(synthetic_proc, synthetic_raw%nrecords, .false.)
+
+!-----------------------------------------------------------------------------------
+! LOOOP THROUGH PAIRS of OBSERVED AND SYNTHETIC TRACES
+!-----------------------------------------------------------------------------------
+  do irecord = 1, observed_raw%nrecords
+    print *, "Processing record ", irecord, " of ", observed_raw%nrecords, " on processor " , rank, "."
+    print *, "record_name: ", trim(observed_raw%receiver_name_array(irecord))&
+      //"."//trim(observed_raw%network_array(irecord))&
+      //"."//trim(observed_raw%receiver_id_array(irecord))&
+      //"."//trim(observed_raw%component_array(irecord))
+
+  !-----------------------------------------------------------------------------------
+  ! CUT OBSERVED AND SYNTHETIC SEISMOGRAM
+  !-----------------------------------------------------------------------------------
+    print *, "Cut begin...rank:", rank
+    !copy trace info to local var
+    b_obsd=sngl(observed_raw%begin_value(irecord))
+    dt_obsd=sngl(observed_raw%sample_rate(irecord))
+    npts_obsd=observed_raw%npoints(irecord)
+    obsd(1:npts_obsd)=sngl(observed_raw%records(irecord)%record(1:npts_obsd))
+
+    b_synt=sngl(synthetic_raw%begin_value(irecord))
+    dt_synt=sngl(synthetic_raw%sample_rate(irecord))
+    npts_synt=synthetic_raw%npoints(irecord)
+    if (npts_synt .eq. 1) cycle
+    synt(1:npts_synt)=sngl(synthetic_raw%records(irecord)%record(1:npts_synt))
+
+    sta=observed_raw%receiver_name_array(irecord)
+    net=observed_raw%network_array(irecord)
+    locid=observed_raw%receiver_id_array(irecord)
+    cha=observed_raw%component_array(irecord)
+    print *, locid, cha
+    !call wsac1("adjust_event_time.obs", obsd, npts_obsd, &
+    !      b_obsd, dt_obsd, nerr)
+    !call wsac1("adjust_event_time.syn", synt, npts_synt, &
+    !      b_synt, dt_synt, nerr)
+
+    call cut_seis(obsd, npts_obsd, dt_obsd, b_obsd, synt, npts_synt, dt_synt, b_synt, &
+    obsd_cut, npts_obsd_cut, dt_obsd_cut, b_obsd_cut, &
+    synt_cut, npts_synt_cut, dt_synt_cut, b_synt_cut)
+    print *, "Cut done...rank:", rank
+
+    !call wsac1("cut.obs", obsd_cut, npts_obsd_cut, &
+    !      b_obsd_cut, dt_obsd_cut, nerr)
+    !call wsac1("cut.syn", synt_cut, npts_synt_cut, &
+    !      b_synt_cut, dt_synt_cut, nerr)
+
+  !-----------------------------------------------------------------------------------
+  ! PREPARE FOR DECONVOLUTION: rmean/rtrend/taper
+  !-----------------------------------------------------------------------------------
+    mean=0
+    slope=0
+    yint=0
+    !call extrma(obsd_cut, increment, num, minnm, maxnm, mean)
+    !call rmean(obsd_cut, npts_obsd_cut, mean)
+    call demean(obsd_cut, npts_obsd_cut, mean)
+    !call lifite(b_obsd_cut, dt_obsd_cut, obsd_cut, npts_obsd_cut,&
+    !      slope, yint, siga, sigb, sig, cc)
+    call detrend(obsd_cut, npts_obsd_cut, yint, slope, &
+            b_obsd_cut, dt_obsd_cut)
+    call taper_width_to_points(.05, sngl(npts_obsd_cut), ipts)
+    call taper(obsd_cut, npts_obsd_cut, 2, ipts)
+    print *, "npoints:", npts_obsd_cut
+    print *, "ipts:", ipts
+    print *, "mean:", sum(obsd_cut(1:npts_obsd_cut))/npts_obsd_cut
+    print *, "npts:", npts_obsd_cut
+    print *, "mean, yint, slope:", mean, yint, slope
+
+    mean=0
+    slope=0
+    yint=0
+    !call extrma(synt_cut, increment, num, minnm, maxnm, mean)
+    !call rmean(synt_cut, npts_synt_cut, mean)
+    call demean(synt_cut, npts_synt_cut, mean)
+    !call lifite(b_synt_cut, dt_synt_cut, synt_cut, npts_synt_cut,&
+    !      slope, yint, siga, sigb, sig, cc)
+    call detrend(synt_cut, npts_synt_cut, yint, slope, &
+             b_synt_cut, dt_synt_cut)
+    call taper_width_to_points(.05, sngl(npts_synt_cut), ipts)
+    call taper(synt_cut, npts_synt_cut, 2, ipts)
+
+    !print *, "npts:", npts_synt_cut
+    !print *, "mean, yint, slope:", mean, yint, slope
+
+    !call wsac1("rmean.obs", obsd_cut, npts_obsd_cut, &
+    !      b_obsd_cut, dt_obsd_cut, nerr)
+    !call wsac1("rmean.syn", synt_cut, npts_synt_cut, &
+    !      b_synt_cut, dt_synt_cut, nerr)
+
+    print *, "Taper done and ready to decon. Rank:", rank
+
+    !call wsac1("before_filter.syn", synt_cut, npts_synt_cut, &
+    !      b_synt_cut, dt_synt_cut, nerr)
+
+    !-----------------------------------------------------------------------------------
+    ! REMOVE INSTRUMENT RESPONSE
+    !-----------------------------------------------------------------------------------
+    call remove_response(obsd_cut, npts_obsd_cut, dt_obsd_cut, &
+      sta, net, cha, locid, observed_raw%responses(irecord)%response_string, &
+      observed_raw%responses(irecord)%response_length)
+    call filter(synt_cut, npts_synt_cut, dt_synt_cut)
+
+    !call wsac1("filter.obs", obsd_cut, npts_obsd_cut, &
+    !      b_obsd_cut, dt_obsd_cut, nerr)
+    !call wsac1("filter.syn", synt_cut, npts_synt_cut, &
+    !      b_synt_cut, dt_synt_cut, nerr)
+    print *, "Response removed. Rank:", rank
+
+    !-----------------------------------------------------------------------------------
+    ! rmean/rtrend/taper again in case instrument response introduces artifacts
+    !-----------------------------------------------------------------------------------
+
+    call demean(obsd_cut, npts_obsd_cut, mean)
+    !call lifite(b_obsd_cut, dt_obsd_cut, obsd_cut, npts_obsd_cut,&
+    !      slope, yint, siga, sigb, sig, cc)
+    call detrend(obsd_cut, npts_obsd_cut, yint, slope, &
+               b_obsd_cut, dt_obsd_cut)
+    call taper_width_to_points(.05, sngl(npts_obsd_cut), ipts)
+    call taper(obsd_cut, npts_obsd_cut, 2, ipts)
+
+    !call demean(synt_cut, npts_synt_cut, mean)
+    !call lifite(b_synt_cut, dt_synt_cut, synt_cut, npts_synt_cut,&
+    !      slope, yint, siga, sigb, sig, cc)
+    !call detrend(synt_cut, npts_synt_cut, yint, slope, &
+    !            b_synt_cut, dt_synt_cut)
+    !call taper_width_to_points(.05, sngl(npts_synt_cut), ipts)
+    !call taper(synt_cut, npts_synt_cut, 2, ipts)
+    !print *, "prepared for deconvolution"
+    !call wsac1("taper.syn", synt_cut, npts_synt_cut,&
+    !      b_synt_cut, dt_synt_cut, nerr)
+    !call wsac1("taper.obs", obsd_cut, npts_obsd_cut,&
+             !b_obsd_cut, dt_obsd_cut, nerr)
+
+    print *, "Second Taper done. Rank:", rank
+
+  !-----------------------------------------------------------------------------------
+  ! RESAMPLE OBSERVED AND SYNTHETIC SEISMOGRAMS TO SAME SAMPLE RATE
+  !-----------------------------------------------------------------------------------
+
+    end_time_syn=b_synt_cut+dt_synt_cut*float(npts_synt_cut)
+    end_time_obs=b_obsd_cut+dt_obsd_cut*float(npts_obsd_cut)
+    b_synt_interp=b_synt_cut
+    b_obsd_interp=b_obsd_cut
+    dt_synt_interp = synthetic_raw%sample_rate(irecord)
+    dt_obsd_interp = synthetic_raw%sample_rate(irecord)
+    npts_synt_interp = int(npts_synt_cut*dt_synt_cut/dt_synt_interp)
+    npts_obsd_interp = int(npts_obsd_cut*dt_obsd_cut/dt_obsd_interp)
+    call interp(synt_cut, npts_synt_cut, synt_interp, npts_synt_interp,&
+                b_synt_cut, end_time_syn, dt_synt_cut, &
+                b_synt_interp, dt_synt_interp, epsi)
+    call interp(obsd_cut, npts_obsd_cut, obsd_interp, npts_obsd_interp,&
+                b_obsd_cut, end_time_obs, dt_obsd_cut, &
+                b_obsd_interp, dt_obsd_interp, epsi)
+
+    print *, "Resample complete. Rank:", rank
+    !call wsac1("final.syn", synt_interp, npts_synt_interp,&
+    !      b_synt_interp, dt_synt_interp, nerr)
+    !call wsac1("final.obs", obsd_interp, npts_obsd_interp,&
+    !         b_obsd_interp, dt_obsd_interp, nerr)
+
+    !-----------------------------------------------------------------------------------
+    ! FINAL CUT OF OBSERVED AND SYNTHETIC SEISMOGRAMS
+    !-----------------------------------------------------------------------------------
+
+    call cut_seis(obsd_interp, npts_obsd_interp, dt_obsd_interp, b_obsd_interp, &
+      synt_cut, npts_synt_interp, dt_synt_interp, b_synt_interp, &
+      obsd_final, npts_obsd_final, dt_obsd_final, b_obsd_final, &
+      synt_final, npts_synt_final, dt_synt_final, b_synt_final)
+    print *, "final cut finished"
+!    
+    observed_proc%begin_value(irecord)=b_obsd_interp
+    observed_proc%sample_rate(irecord)=dt_obsd_interp
+    observed_proc%npoints(irecord)=npts_obsd_interp
+    allocate(observed_proc%records(irecord)%record(npts_obsd_interp))
+    observed_proc%records(irecord)%record(1:npts_obsd_interp)=dble(obsd_interp(1:npts_obsd_interp))
+!
+    synthetic_proc%begin_value(irecord)=b_synt_interp
+    synthetic_proc%sample_rate(irecord)=dt_synt_interp
+    synthetic_proc%npoints(irecord)=npts_synt_interp
+    allocate(synthetic_proc%records(irecord)%record(npts_synt_interp))
+    synthetic_proc%records(irecord)%record(1:npts_synt_interp)=dble(synt_interp(1:npts_synt_interp))
+    !endif
+  enddo
+
+  !-----------------------------------------------------------------------------------
+  ! COPY METADATA TO ASDF CONTAINER
+  !-----------------------------------------------------------------------------------
+  call store_asdf_metadata(observed_raw, observed_proc)
+  call store_asdf_metadata(synthetic_raw, synthetic_proc)
+
+  !-----------------------------------------------------------------------------------
+  ! Write out file before rotate
+  !----------------------------------------------------------------------------------- 
+  call write_asdf_file("obs.bp", observed_proc, adios_group, rank, nproc, comm, ierr)
+  call write_asdf_file("syn.bp", synthetic_proc, adios_group, rank, nproc, comm, ierr)
+
+  !-----------------------------------------------------------------------------------
+  ! ROTATE
+  !----------------------------------------------------------------------------------- 
+  print *, "Rotation begin, rank:", rank
+  call rotate_traces(observed_proc, observed_rotate)
+  call rotate_traces(synthetic_proc, synthetic_rotate)
+  print *, "Rotation done rank:", rank
+
+end subroutine process_asdf
+
+end module
---
+++ src/preprocess/process_subs2.f90
@@ -0,0 +1,317 @@
+module process_subs2
+
+  implicit none
+
+  double precision :: FLT_MIN=1e-30
+
+contains
+
+!======================================================================
+!> Removes the instrument response from the observed seismogram
+!! \param observed The observed seismogram
+!! \param synthetic The synthetichetic seismogram
+subroutine remove_response(obsd, npts_obsd, dt_obsd, sta, net, cha, locid, &
+  response, resp_length)
+
+  use var_main
+  use process_var
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  real,dimension(*),intent(inout):: obsd
+  integer, intent(in) :: npts_obsd
+  real(kind=4),intent(in)   :: dt_obsd
+  character(len=*) :: sta, net, cha, locid, response
+  integer :: resp_length
+
+  !local var
+  integer :: LNPT, FFT_NPTS
+  integer :: i, j, ierr
+
+  !evresp var
+  character*200        :: datime
+  character(len=200)   :: temp_resp_fn
+  character(len=10)    :: units, rtype
+  integer              :: start_stage=-1,stop_stage=0,stdio_flag=0
+  integer              :: lsta, lnet, lcha, llocid, ldatime, lunits, lfile, lrtype, lverbose
+  integer              :: useTotalSensitivityFlag
+
+  character*20         :: datime2,datime3
+
+  integer              :: iflag, evresp
+  character*10         :: verbose
+  real(kind=4), allocatable    :: resp(:), freq(:)
+  
+  double precision :: denr
+
+  !transfer
+  real(kind=8), allocatable :: s_re(:), s_im(:)
+  real(kind=8), allocatable :: x_re(:), x_im(:)
+  real(kind=8)              :: f1, f2
+  real(kind=8)              :: f3, f4
+  real(kind=8), dimension(4):: F
+  integer                   :: nfreq
+  double precision          :: delfrq
+
+  !filter
+  real(kind=8)         :: unused=0.0, unused2=0.0
+  real*8  :: dt_temp
+
+  start_stage=-1
+  stop_stage = 0
+  stdio_flag=0
+  if (npts_obsd .ge. 524288) then
+    LNPT = 20
+    FFT_NPTS = 2**LNPT
+  elseif ( npts_obsd .ge. 262154) then
+    LNPT = 19
+    FFT_NPTS = 2**LNPT
+  else
+    LNPT = 18
+    FFT_NPTS = 2**LNPT
+  endif
+  allocate(freq(FFT_NPTS))
+  allocate(s_re(FFT_NPTS))
+  allocate(s_im(FFT_NPTS))
+  allocate(x_re(FFT_NPTS))
+  allocate(x_im(FFT_NPTS))
+  allocate(resp(FFT_NPTS*2))
+  !allocate(observed_cmplx(FFT_NPTS))
+  !allocate(synthetic_cmplx(FFT_NPTS))
+
+  !observed_dt = observed_raw%sample_rate(irecord)
+
+  !+++++++++++++++++++++++++++++++++++
+  !evresp
+  do i = 1, FFT_NPTS
+    freq(i) = sngl(i-1)/FFT_NPTS/dt_obsd
+  enddo
+  freq(1) = eps
+
+  !call create_temp_resp_file(sta, cha, net, locid, response, resp_length,&
+  !        temp_resp_fn, ierr)
+  !------------------------------------------------
+  ! DECONVOLUTION: EVALRESP to compute instrument response
+  ! FIX 32-64 bit conflict between SAC and EVALRESP: USE 64-bit SAC
+  !-------------------------------------------------
+  !length = observed_raw%responses(irecord)%response_length
+  write(datime2,*) gmt_year
+  write(datime3,*) gmt_day+1 ! need to make sure the event is afer this date
+  datime = trim(adjustl(datime2))//","//trim(adjustl(datime3))
+  units='DIS'
+  verbose='-v'
+  rtype = 'CS'
+  lsta=len_trim(sta); lcha=len_trim(cha); lnet=len_trim(net)
+  llocid=len_trim(locid)
+  ldatime=len_trim(datime)
+  lunits=len_trim(units)
+  lfile=len_trim(temp_resp_fn)
+  !lfile=16
+  lrtype=len_trim(rtype)
+  lverbose=len_trim(verbose)
+  !print *,"response: ", trim(response) 
+  !print *,"end response"
+  !temp_resp_fn=adjustl(temp_resp_fn)
+  !print *, "enter..."
+  !print *, "sta:", trim(sta), trim(cha), trim(net), trim(locid), ' || ',trim(datime)
+  !print *, "resp_file:", trim(temp_resp_fn), "|||"
+  !print *, lsta, lcha, lnet, llocid, ldatime
+  !print *, lunits, lfile, lrtype, lverbose
+  !print *, "FFT_NPTS:", FFT_NPTS
+  !print *, "stdio_flag:", stdio_flag
+
+  nfreq=FFT_NPTS
+  !********
+  !test block
+  !nfreq=91
+  !do i=1,91
+  !  freq(i)=0.01+0.001*(i-1)
+  !enddo
+  iflag = evresp(sta, cha, net, locid, datime, &
+            units, response(1:resp_length), freq, nfreq, resp, &
+            rtype, verbose, start_stage, stop_stage, &
+            stdio_flag)!, lsta, lcha, lnet, llocid, &
+  
+  !iflag = evresp(sta, cha, net, locid, datime, &
+  !          units, temp_resp_fn, freq, nfreq, resp, &
+  !          rtype, verbose, start_stage, stop_stage, &
+  !          stdio_flag)!, lsta, lcha, lnet, llocid, &
+            !ldatime, lunits, lfile, lrtype, lverbose, useTotalSensitivityFlag)
+
+  !call delete_temp_resp_file(temp_resp_fn)
+  !write out spectrum
+  !open(unit=111, file="spec.log")
+  !do i=1,nfreq
+  !  write(111,*) freq(i), resp(2*i-1), resp(2*i)
+  !enddo
+  !close(111)
+
+  print *, "checkpoint: ",trim(sta), trim(cha),trim(net), trim(locid)
+  !deallocate(response)
+
+  if (iflag.ne.0) then
+    write(*,*) 'ERROR computing instrument response:'
+    do i = 1, FFT_NPTS
+      resp(i)=cmplx(1.0,0.0)
+    enddo
+  endif
+  j = 1
+  do i = 1, FFT_NPTS
+    s_re(i) = dble(resp(j))
+    s_im(i) = dble(resp(j+1))
+    j = j + 2
+  enddo
+
+  ! Compute transfer function
+  ! 1.0/ 'FROM' FROM = Sre + iSim
+  ! Set to zero if less than FLT_MIN
+  do i=1, nfreq
+    denr=(s_re(i)**2+s_im(i)**2)
+    if(denr.lt.FLT_MIN)then
+      s_re(i)=0.0
+      s_im(i)=0.0
+    else
+      denr=1.0/denr
+      s_re(i)=s_re(i)*denr
+      s_im(i)=-s_im(i)*denr
+    endif
+  enddo
+
+  !+++++++++++++++++++++
+  !transfer
+  f3 = 1.0/MIN_PERIOD
+  f2 = 1.0/MAX_PERIOD
+  f1 = f2 * 0.8
+  f4 = f3 * 1.2
+  F = (/ f1, f2, f3, f4 /)
+  x_re(1:FFT_NPTS)=1.0
+  x_im(:)=0.0
+  delfrq = 1.0/(dble(FFT_NPTS)*dble(dt_obsd))
+  !print *, npts_obsd
+  !open(unit=111, file="xre1.log")
+  !do i=1,FFT_NPTS
+  !  write(111,*) x_re(i), x_im(i), s_re(i), s_im(i)
+  !enddo
+  !close(111)
+  nfreq = FFT_NPTS/2+1
+  call ztransfer(obsd(1:npts_obsd), npts_obsd, dble(dt_obsd), s_re, s_im, &
+          x_re, x_im, nfreq, FFT_NPTS, delfrq, F)
+  !open(unit=111, file="xre2.log")
+  !do i=1,FFT_NPTS
+  !  write(111,*) x_re(i), x_im(i), s_re(i), s_im(i)
+  !enddo
+  !close(111)
+  
+
+  print *, "transfer done"
+  !transfer from nanometer to meter
+  obsd(1:npts_obsd) = s_re(1:npts_obsd)!*1e-9
+
+  !++++++++++++++++++++++
+  !filter data
+  dt_temp=dt_obsd
+  !call xapiir(obsd, npts_obsd, 'BU',unused, unused, 4, 'BP', &
+  !        f2, f3, dt_temp, 2)
+
+  !print *, "remove instrument response done!"
+
+end subroutine remove_response
+
+subroutine filter(trace, npts, dt)
+
+  use var_main
+
+  real(kind=4), dimension(*) :: trace
+  integer :: npts
+  real(kind=4) :: dt
+
+  double precision :: dt_temp
+  double precision :: f1, f4,f3, f2, unused=0.0, unused2=0.0
+  real(kind=8), allocatable :: s_re(:), s_im(:)
+  real(kind=8), allocatable :: x_re(:), x_im(:)
+  real(kind=8), dimension(4):: F
+  integer                   :: nfreq
+  double precision          :: delfrq
+
+  integer FFT_NPTS, LNPT
+  
+  if (npts .ge. 524288) then
+    LNPT = 20
+    FFT_NPTS = 2**LNPT
+  elseif ( npts .ge. 262154) then
+    LNPT = 19
+    FFT_NPTS = 2**LNPT
+  else
+    LNPT = 18
+    FFT_NPTS = 2**LNPT
+  endif
+
+  allocate(s_re(FFT_NPTS))
+  allocate(s_im(FFT_NPTS))
+  allocate(x_re(FFT_NPTS))
+  allocate(x_im(FFT_NPTS))
+  f3 = 1.0/MIN_PERIOD
+  f2 = 1.0/MAX_PERIOD
+  dt_temp=dble(dt)
+
+  !+++++++++++++++++++++
+  !transfer
+  f3 = 1.0/MIN_PERIOD
+  f2 = 1.0/MAX_PERIOD
+  f1 = f2 * 0.8
+  f4 = f3 * 1.2
+  F = (/ f1, f2, f3, f4 /)
+  x_re(1:FFT_NPTS)=1.0
+  x_im(:)=0.0
+  s_re(1:FFT_NPTS)=1.0
+  s_im(:)=0.0
+  delfrq = 1.0/(dble(FFT_NPTS)*dble(dt))
+  !print *, npts
+  nfreq = FFT_NPTS/2+1
+  call ztransfer(trace, npts, dble(dt), s_re, s_im, &
+          x_re, x_im, nfreq, FFT_NPTS, delfrq, F)
+  trace(1:npts) = s_re(1:npts)!*1e-9
+
+
+  !print *, "sum synt:", sum(trace(1:npts))
+  !print *, "npts:", npts
+  !print *, "dt_synt:", dt
+  !print *, "f2, f3:", f2, f3
+
+  !call xapiir(trace, npts, "BU", &
+  !        unused, unused2, 4, 'BP', f2, f3, dt_temp, 2)
+
+end subroutine filter
+
+!Creat temporary resp file
+subroutine create_temp_resp_file(sta, cha, net, locid, response, resp_length, &
+               temp_resp_fn, ierr)
+
+  character(len=*) :: sta, cha, net, locid, response, temp_resp_fn
+  integer :: ierr, resp_length
+
+  character(len=60) :: temp_dir=''
+
+  temp_resp_fn=''
+
+  temp_resp_fn=trim(temp_dir)//'RESP.'//trim(net)//'.'//trim(sta)//&
+    '.'//trim(locid)//'.'//trim(cha)
+
+  !print *, "temp_file: ",trim(temp_resp_fn)
+  !print *, "len:",len_trim(temp_resp_fn)
+  open(unit=111, file=temp_resp_fn, iostat=ierr)
+  write(111, '(a)') response(1:resp_length)
+  close(111)
+
+end subroutine create_temp_resp_file
+
+!Delete temporary resp file
+subroutine delete_temp_resp_file(resp_fn)
+
+  character(len=*) :: resp_fn
+
+  call system('rm '//trim(resp_fn)//'')
+
+end subroutine delete_temp_resp_file
+
+end module process_subs2
